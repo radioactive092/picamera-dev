5aa88e23d978ca0f257a1926896863671d0fe2d5
radioactive092 <saptarshikundu1995@gmail.com>
2018-04-12 17:56:26 +0530

    v0.5

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	session_start();
+
+	include('./loginsystem/login_action.php');
+	if(isset($_SESSION['username'])) {
+		include('./loginsystem/dashboard.php');
+		var_dump($_SESSION);
+	} else {
+		include('./loginsystem/login.php');
+	}
+?>
--- loginsystem -nf/test.py
+++
@@ -1,9 +0,0 @@
-class a :
-    def __init__(self,b) :
-        c=b
-
-    def method1(self) :
-        print(c)
-
-obj=a(5)
-obj.method1()
