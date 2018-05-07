26ca44c3fdc02ddc40a6815e355b350fb24ea848
radioactive092 <saptarshikundu1995@gmail.com>
2018-05-07 18:49:36 +0530

    v0.6

--- loginsystem/servicestatus.php
+++ loginsystem/servicestatus.php
@@ -2,6 +2,18 @@
 
 $servicestatus=$_POST['servicestatus'];
 
+if($servicestatus=="restart")
+{
+  $output = shell_exec("./restartservice.sh");
+}
+elseif($servicestatus=="start")
+{
+  $output = shell_exec("./startservice.sh");
+}
+else
+{
+  $output = shell_exec("./stopservice.sh");
+}
 
 
 ?>
