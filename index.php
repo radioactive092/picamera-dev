dfbd11413b0cc2fe553862cebaad017f7d0735d4
radioactive092 <saptarshikundu1995@gmail.com>
2018-05-09 12:05:53 +0530

    barnik da

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 
-	include('./loginsystem/login_action.php');
+	include('./login_action.php');
 	if(isset($_SESSION['username'])) {
 		include('./loginsystem/dashboard.php');
 //		var_dump($_SESSION);
--- loginsystem/logout.php
+++ loginsystem/logout.php
@@ -1,6 +1,6 @@
-<?PHP
+<?php
 session_start();
 session_destroy();
-header('Location: login.php');
+header('Location: /index.php');
 exit;
 ?>
